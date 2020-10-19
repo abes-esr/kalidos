b0cf86bda861c9c3d72017fe968d4b0c5b5c8ab9
Monteix Yann <yann.monteix@etu.univ-lyon1.fr>
2020-10-19 16:50:16 +0200

    resultat dans fichier json

--- serveur/index.js
+++ serveur/index.js
@@ -1,14 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 
 
 
 const app = express();
 const path = require('path');
+const fs = require('fs');
 
 
 
 const DIST_DIR = path.join(__dirname, '../dist');
-
+app.use(bodyParser.json())
 app.use(express.static(DIST_DIR));
 const port = process.env.PORT || 3000;
 const mockResponse = {
@@ -19,6 +21,12 @@ const mockResponse = {
 app.get('/rules', (req,res) => {
   const file = `${__dirname}/public/model_regles.json`;
   res.download(file); // Set disposition and send it.
+});
+app.post('/rules', (req,res) => {
+
+  res.sendStatus(200);
+  const json = JSON.stringify(req.body);
+  fs.writeFile(`${__dirname}/public/result.json`, json, 'utf8' , () => {});
 });
 
 app.post('/upload', (req,res) => {
