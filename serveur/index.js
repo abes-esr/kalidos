f074589a1df1ca4126c2d164a97314f0daffa5b4
Monteix Yann <yann.monteix@etu.univ-lyon1.fr>
2020-10-19 22:58:26 +0200

    ajout de regles en dur et rajout de la requete de generation de resultat

--- serveur/index.js
+++ serveur/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const _ = require('lodash');
 
 
 
@@ -22,16 +23,48 @@ app.get('/rules', (req,res) => {
   const file = `${__dirname}/public/model_regles.json`;
   res.download(file); // Set disposition and send it.
 });
-app.post('/rules', (req,res) => {
 
-  res.sendStatus(200);
+
+app.post('/result', (req,res) => {
   const json = JSON.stringify(req.body);
   fs.writeFile(`${__dirname}/public/result.json`, json, 'utf8' , () => {});
+  res.sendStatus(200);
+});
+
+
+app.post('/rules', (req,res) => {
+  let json = fs.readFileSync(`${__dirname}/public/model_regles.json`);
+  let rules = JSON.parse(json);
+  const type = req.header("type");
+  const categorie = req.header("categorie");
+  let valid = true;
+
+  rules[categorie][type].forEach(function(element){
+    if(_.isEqual(element, req.body)){
+      valid = false;
+      res.sendStatus(200);
+    }
+  });
+
+  if(valid) {
+    rules[categorie][type].push(req.body)
+  }
+
+
+  console.log(rules[categorie][type]);
+
+  const newJson = JSON.stringify(rules);
+  fs.writeFile(`${__dirname}/public/model_regles.json`, newJson, 'utf8' , () => {});
+
+
+  res.sendStatus(200);
 });
 
 app.post('/upload', (req,res) => {
   res.sendStatus(200);
 });
+
+
 
 
 
--- src/js/verifyRules.js
+++ src/js/verifyRules.js
@@ -1,16 +1,20 @@
 const axios = require('axios');
-const xml2js = require('xml2js');
 const convert = require("xml-js");
 
 let result = {};
 
+const CATEGORIE = "Generale";
+const TYPE = "matching";
+const NEWRULE = {
+    "number": 404,
+    "code" : "z",
+    "message" : "NEW RULE",
+    "regex" : "[A-Z]*[a-z]+"
+}
 
 function verifyRules() {
     var rules = getRules()
-
     var obj;
-
-
 }
 
 function getSudoc(rules,PPN) {
@@ -31,7 +35,48 @@ function getSudoc(rules,PPN) {
         });
 }
 
+function writeResult(){
+    axios({
+        method: 'POST',
+        url: 'http://localhost:3000/result',
+        contentType: "application/json",
+        headers: {
+            "Accept": "application/json",
+        },
+        data: result,
+    }).then(function () {
+        console.log("ok")
+    })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .then(function () {
+        });
+}
 
+
+function addRule(categorie,type,rule){
+    axios({
+        method: 'POST',
+        url: 'http://localhost:3000/rules',
+        contentType: "application/json",
+        headers: {
+            "Accept": "application/json",
+            "categorie" : categorie,
+            "type" : type
+        },
+        data: rule,
+    }).then(function () {
+        console.log("ok")
+    })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .then(function () {
+        });
+}
 
 function getRules(PPN) {
     axios.get('http://localhost:3000/rules')
@@ -74,6 +119,8 @@ function verifMain(rules,sudoc) {
     });
     result[controlfields[0]._text]= resultJson;
     console.log(result);
+    writeResult();
+    addRule(CATEGORIE,TYPE,NEWRULE)
 }
 
 export default verifyRules;
