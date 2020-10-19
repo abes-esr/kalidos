2a12fd5b10d9f465ef612e0f90824ab3d028b845
p2019045 <karim.abdi@etu.univ-lyon1.fr>
2020-10-19 14:21:07 +0200

    matching regle verif

--- serveur/index.js
+++ serveur/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const xml2js = require('xml2js');
+
 
 const app = express();
 const path = require('path');
 
-const fs = require('fs');  
+const fs = require('fs');
+
 
 const DIST_DIR = path.join(__dirname, '../dist');
 
@@ -13,6 +16,46 @@ const mockResponse = {
   foo: 'bar',
   bar: 'foo',
 };
+
+var obj = JSON.parse(fs.readFileSync('model_regles.json', 'utf8'));
+
+var parser = new xml2js.Parser();
+let xml_string = fs.readFileSync("notice.xml", "utf8");
+
+var leader;
+var controlfields;
+var datafields;
+
+
+parser.parseString(xml_string, function(error, result) {
+  if(error === null) {
+    leader = result.record.leader;
+    controlfields = result.record.controlfield ;
+    datafields = result.record.datafield;
+  }
+  else {
+    console.log(error);
+  }
+});
+
+
+ // console.log(leader);
+  //console.log(controlfields);
+  //console.log(datafields);
+
+obj.Generale.matching.forEach(function(regle) {
+  const regex = RegExp(regle.regex);
+  datafields.forEach(function (field){
+       if(field['$'].tag == regle.Number){
+         field.subfield.forEach(function (subfield){
+              if(subfield['$'].code === regle.code && !regex.test(subfield._))
+                 console.log(regle.message +" : "+  subfield._);
+         });
+       }
+  });
+});
+
+
 
 app.get('/api', (req, res) => {
   res.send(mockResponse);
